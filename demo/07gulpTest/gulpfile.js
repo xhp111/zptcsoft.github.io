572152503aaa8d47e8877bdfadd7b8c21083718b
whqet <whqet@qq.com>
2017-04-06 13:27:48 +0800

    20170406 add gulpTest

--- demo/07gulpTest/gulpfile.js
+++ demo/07gulpTest/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     cssbeautify = require('gulp-cssbeautify'),
-    cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');;
 
 gulp.task('default', function() {
   // 将你的默认的任务代码放在这
@@ -9,6 +11,10 @@ gulp.task('default', function() {
 gulp.task('cssB', function() {
     return gulp.src('./css/*.css')
         .pipe(cssbeautify())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./css/'));;
 });
 
@@ -18,7 +24,18 @@ gulp.task('cssM', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
+gulp.task('sass', function () {
+  return gulp.src('./css/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
 var watcher = gulp.watch('css/**/*.css', ['cssB','cssM']);
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
+
+var watcherScss = gulp.watch('css/**/*.scss', ['sass']);
 watcher.on('change', function(event) {
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 });
